34c4414736305bc35a4cdd9b9979280ed759a9d4
tyguy310 <tyler_maier@yahoo.com>
2016-12-05 16:29:11 -0700

    deploy

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
     }
   },
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
